45eb7cc2179c4eee5229a96aa8531874db5a8790
Dicee <dcourtinot@gmail.com>
2015-06-14 19:11:39 +0100

    Refactored Dist to make it more readable and concise

--- benj/Dist.java
+++ benj/Dist.java
@@ -1,154 +1,135 @@
 package benj;
 
+import static benj.math.MathUtils.isZero;
 import static benj.utils.FileUtils.canonicalCurrentDirectory;
 import static benj.utils.FileUtils.ensureExists;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import javafx.util.Pair;
 
 import javax.swing.JOptionPane;
 
-import benj.math.ArrayIndexComparator;
-import benj.math.TriMatrice;
+import benj.math.MathUtils;
+import benj.math.Matrix;
+import benj.math.SimpleMatrix;
+import benj.math.TriMatrix;
 import benj.utils.FileUtils;
 
 public class Dist {	
-	public static TriMatrice<String> mat(String outPath) throws IOException {
+	private static final int INFINITY = 100000;
 	
-//		InputStreamReader isr = new InputStreamReader(Dist.class.getResourceAsStream("dist.txt"));
-		
-		FileReader dist = new FileReader(String.format(outPath+"/data/dist.txt"));
-		
-		BufferedReader brDist = new BufferedReader(dist);
-		Pattern p = Pattern.compile(";");
-		
-		String title=brDist.readLine();
-		
-		String[] obj = p.split(title);
-			
-		int n=obj.length;
-				
-		TriMatrice<String> distMat = new TriMatrice<String>(n,String.class);
-		
-		for(int j=0;j<n;j++)
+	public static TriMatrix<String> mat(String outPath) throws IOException {
+		try (BufferedReader br = new BufferedReader(new InputStreamReader(Dist.class.getResourceAsStream(outPath + "/data/dist.txt")))) {
+			Pattern           separator = Pattern.compile(";");
+			String            title     = br.readLine();		
+			String[]          obj       = separator.split(title);
+			TriMatrix<String> distMat   = initDistanceMatrix(obj);		
+			populateDistanceMatrix(br,separator,obj,distMat);	
+			return distMat;
+		}
+	}
+
+	private static TriMatrix<String> initDistanceMatrix(String[] obj) {
+		TriMatrix<String> distMat = new TriMatrix<String>(obj.length,String.class);
+		for(int j=0 ; j<obj.length ; j++) 
 			distMat.set(0,j,obj[j]);
-		
-		int i=1;
+		return distMat;
+	}
+
+	private static void populateDistanceMatrix(BufferedReader br, Pattern p, String[] obj, TriMatrix<String> distMat) throws IOException {
+		int i = 1;
 		String line;
-		while((line=brDist.readLine())!=null){
+		while((line = br.readLine()) != null){
 			String[] tmp = p.split(line);
-			for(int j=i;j<n;j++)
+			for(int j=i ; j<obj.length ; j++)
 				distMat.set(i,j,tmp[j-i]);
 			i++;
 		}
-				
-		brDist.close();	
-		return distMat;
 	}
 		
-	public static String[][] find(String which, int max, String outPath) throws IOException{
-		TriMatrice<String> distMat = mat(outPath);
-		List<String> obj = distMat.getRow(0);
-		List<Float> obj2 = new ArrayList<>();
-		int where = obj.indexOf(which);
-		int N = obj.size();
-		for(int i=1;i<=where;i++){
-			obj2.add(Float.parseFloat(distMat.get(i,where)));
+	public static Matrix<String> find(String which, int scope, String outPath) throws IOException{
+		TriMatrix<String> distMat  = mat(outPath);
+		List<String>      firstRow = distMat.getRow(0);
+		
+		List<Float>                values          = fillWithNeighbours(which,distMat,firstRow);
+		List<Pair<Integer, Float>> sortedWithIndex = sortWithIndex(values);
+		 
+		return closestNeighbours(scope,firstRow,sortedWithIndex);
+	}
+
+	private static Matrix<String> closestNeighbours(int max, List<String> firstRow, List<Pair<Integer, Float>> sortedWithIndex) {
+		Matrix<String> res = new SimpleMatrix<>(2,max,String.class);
+		for (int k=0 ; k<max ; k++) {
+			Pair<Integer,Float> pair = sortedWithIndex.get(k);
+			res.set(0,k,firstRow.get(pair.getKey()))
+			   .set(1,k,String.valueOf(pair.getValue()));
 		}
-		for(int j=where+1;j<N;j++){
-			System.out.println("le j "+j);
-			System.out.println(distMat.get(where+1,j));
-			obj2.add(Float.parseFloat(distMat.get(where+1,j)));
-		}
-		obj.remove(which);
-		
-		Float [] length = new Float[N-1];
-		
-		for(int k=0;k<N-1;k++){
-			length[k]=obj2.get(k);
-		}
-		
-		ArrayIndexComparator comparator = new ArrayIndexComparator(length);
-		Integer[] indexes = comparator.createIndexArray();
-		Arrays.sort(indexes, comparator);
-		Arrays.sort(length);
-		
-		String[][] res = new String[max][max];
-		
-		for(int k=0;k<max;k++){
-			res[0][k]=obj.get(indexes[k]);
-			res[1][k]=Float.toString(length[k]);
-		}
-		
 		return res;
 	}
-	
-	public static String calculWeight(String toParse)throws IOException{
-		String res;
-		float tmp=Float.parseFloat(toParse);
-		if(tmp==0.0)
-			res="1";
-		else if(tmp<0.4)
-			res="0.7";
-		else if(tmp<0.7)
-			res="0.7";
-		else
-			res="0.7";
-		return res;
+
+	private static List<Pair<Integer, Float>> sortWithIndex(List<Float> values) {
+		return IntStream.range(0,values.size())
+			.mapToObj(i -> new Pair<>(i,values.get(i)))
+			.sorted((p0,p1) -> MathUtils.epsilonCompare(p0.getValue(),p1.getValue()))
+			.collect(Collectors.toList());
+	}
+
+	private static List<Float> fillWithNeighbours(String which, TriMatrix<String> mat, List<String> row) {
+		List<Float> values = new ArrayList<>();
+		int         where  = row.indexOf(which);
+		for(int i=1       ; i<=where     ; i++) values.add(Float.parseFloat(mat.get(i      ,where)));
+		for(int j=where+1 ; j<row.size() ; j++) values.add(Float.parseFloat(mat.get(where+1,j    )));
+		row.remove(which);
+		return values;
 	}
 	
-	public static String calculLen(String toParse)throws IOException{
-		String res;
-		float tmp=Float.parseFloat(toParse);
-		res=Float.toString(50*(0.01f+tmp));
-		return res;
+	public static float computeWeight(String toParse) throws IOException{
+		float f = Float.parseFloat(toParse);
+		return isZero(f) ? INFINITY : 1/f;
 	}
 	
-	public static String decideColor(String toParse)throws IOException{
-		String res;
-		float tmp=Float.parseFloat(toParse);
-		if(tmp<=0.3)
-			res="red";
-		else
-			res="blue";
-		return res;			
-	}
-	
-	public static void writeGraph(String which, int max, String inPath, String outPath) throws IOException{
-		String[][] toWrite = find(which, max, outPath);
-		int N=toWrite.length;
-		File out = new File(String.format(outPath+"/graph.neato"));
+	private static void writeGraph(String which, int max, String inPath, String outPath) throws IOException  {
+		Matrix<String> toWrite = find(which, max, outPath);
 		
-    	if(out.exists()){
-    		out.delete();
-    		out.createNewFile();
-    	}
-		
-        FileWriter fw 		   = new FileWriter(out,true);
-        BufferedWriter bw      = new BufferedWriter(fw);
-        
-        bw.write("graph G{\n");
-        bw.write("\tnode[fontsize=5]\n");
-        bw.write("\t[style=filled]\n");
-        bw.write("\t"+"\""+which+"\""+"[fillcolor=\"red\"]\n");
-        bw.write("\t[pack=false]\n");
-        bw.write("\t[defaultdist=0]\n");
-        for(int k=0;k<N;k++){
-        	bw.write("\t"+"\""+which+"\""+"--"+"\""+toWrite[0][k]+"\""+" [len="+calculLen(toWrite[1][k])+"]"+"[weight="+calculWeight(toWrite[1][k])+"][color="+decideColor(toWrite[1][k])+"]\n");
+		File out = new File(outPath + "graph.neato");
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(out))) {
+	        bw.write("graph G{\n");
+	        bw.write("\tnode[fontsize=5]\n");
+	        bw.write("\t[style=filled]\n");
+	        bw.write("\t"+"\""+which+"\""+"[fillcolor=\"red\"]\n");
+	        bw.write("\t[pack=false]\n");
+	        bw.write("\t[defaultdist=0]\n");
+	        for (int k=0 ; k<toWrite.nRows() ; k++)
+	        	bw.write(String.format("\t\"%s\"--\"%s\" [len=%f][weight=%f][color=%s]\n",
+	        			which,
+	        			toWrite.get(0,k),
+	        			computeLen(toWrite.get(1,k)),
+	        			computeWeight(toWrite.get(1,k)),
+	        			decideColor(toWrite.get(1,k))));
+	        bw.write("}");
         }
         
-        bw.write("}");
-        bw.close();
-        ProcessBuilder pb = new ProcessBuilder("neato","-Tpdf",out.getAbsolutePath(),"-o",FileUtils.toExtension(out,".pdf").getAbsolutePath());
+        ProcessBuilder pb = new ProcessBuilder("fdp","-Tpdf",out.getAbsolutePath(),"-o",FileUtils.toExtension(out,".pdf").getAbsolutePath());
         pb.start();
+	}
+	
+	private static String decideColor(String toParse)throws IOException{
+		return Float.parseFloat(toParse) <= 0.3 ? "red" : "blue";
+	}
+	
+	private static float computeLen(String toParse)throws IOException{
+		return 50*(0.01f + Float.parseFloat(toParse));
 	}
 	
 	public static void main(String[] args) throws IOException {
@@ -177,4 +158,3 @@ public class Dist {
 			update.run();
 	}
 }
-	
