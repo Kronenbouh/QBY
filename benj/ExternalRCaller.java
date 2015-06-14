4b157bccf46a35684004b98a5083e5ac6d91fe34
Dicee <dcourtinot@gmail.com>
2015-06-14 19:59:46 +0100

    Minor changes in ExternalRCaller

--- benj/ExternalRCaller.java
+++ benj/ExternalRCaller.java
@@ -12,23 +12,17 @@ public class ExternalRCaller {
 	      caller.setRscriptExecutable("C://Program Files//R//R-3.1.0//bin//x64//Rscript.exe");
 	      
 	      RCode code = new RCode();
-	      
 	      code.clear();
 	      
-	      String code1 = String.format("inPath<-\""+inPath+"\"");
-	      String code2 = String.format("outPath<-\""+outPath+"\"");
-	      
-	      
-	      code.addRCode(code1);
-	      code.addRCode(code2);
-	      
+	      code.addRCode("inPath<-\""  + inPath + "\"");
+	      code.addRCode("outPath<-\"" + outPath + "\"");
 	      code.addRCode("source(paste(inPath,\"//lib//RApi//tools.R\",sep=\"\"))");
 	      code.addRCode("source(paste(inPath,\"//lib//RApi//APImat.R\",sep=\"\"))");
 	      code.addRCode("allMat(outPath)");
 	      
 	      caller.setRCode(code);
 	      try {
-		      caller.redirectROutputToFile(String.format(outPath+"/test.txt"), true);
+		      caller.redirectROutputToFile(outPath + "/test.txt", true);
 		      caller.runOnly();
 	      } catch (IOException e) {
 	    	  throw new RuntimeException(e);
